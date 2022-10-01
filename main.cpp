a5289ff802db10cc836d797242015b1eb3656cc3
Gusion2045 <114810619+Gusion2045@users.noreply.github.com>
2022-10-01 16:05:15 +0300

    Add files via upload
    prima lectie de c++

---
+++ main.cpp
@@ -0,0 +1,59 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+//int g;
+
+
+int main()
+{
+    int i, a, c;
+    int x,X;
+    x=4;
+    X=5;
+// cout<<"valuarea lui x="<<X;
+   // cout<<endl<<"X="<<X;
+    unsigned int e = 4; //numar natural
+    double  d= 4.5;
+    int dd=1;
+    //cout<<d+dd;
+    
+    //char f = 'e';
+    //cout <<f;
+    
+    bool h=1;
+    bool r=false;
+    //cout<<h<<" "<<r;
+   // cout<<"marimea int in memorie este de:"<<sizeof(int)<<"bytes"<<endl;
+    //cout<<"marimea double in memorie este de:"<<sizeof(double)<<"bytes"<<endl;
+   // cout<<"marimea float in memorie este de:"<<sizeof(float)<<"bytes"<<endl;
+   // cout<<"marimea long in memorie este de:"<<sizeof(long)<<"bytes"<<endl;
+   // cout<<"marimea bool in memorie este de:"<<sizeof(bool)<<"bytes"<<endl;
+   // cout<<"marimea unsigned int in memorie este de:"<<sizeof(unsigned int)<<"bytes"<<endl;
+   
+   //cout<<"salut"<<endl<<"mere\n"<<"pere";
+   char ccc = 'A';
+   int nr
+   cout<<(char)nr;
+//    cout<<"introduceti un numar:";
+//    cin>>i;
+//    cout<<i;
+    
+/*    for(i=1;i<5;i++)
+    {
+        if(i==2)
+        {
+            i++;
+        }
+    }
+*/
+    return 0;
+}
+
