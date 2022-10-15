f34fe5672bd80eb37fc4144828a3f2369306f043
Gusion2045 <114810619+Gusion2045@users.noreply.github.com>
2022-10-15 16:06:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,53 +7,118 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <iostream>
-
 using namespace std;
-//int g;
-
 
 int main()
 {
-    int i, a, c;
-    int x,X;
-    x=4;
-    X=5;
-// cout<<"valuarea lui x="<<X;
-   // cout<<endl<<"X="<<X;
-    unsigned int e = 4; //numar natural
-    double  d= 4.5;
-    int dd=1;
-    //cout<<d+dd;
+    int v, g, h;
+    v = 1;
+    g = 7;
+    h = 5;
     
-    //char f = 'e';
-    //cout <<f;
+    //cout<<v<<endl<<g<<endl<<h<<endl;
     
-    bool h=1;
-    bool r=false;
-    //cout<<h<<" "<<r;
-   // cout<<"marimea int in memorie este de:"<<sizeof(int)<<"bytes"<<endl;
-    //cout<<"marimea double in memorie este de:"<<sizeof(double)<<"bytes"<<endl;
-   // cout<<"marimea float in memorie este de:"<<sizeof(float)<<"bytes"<<endl;
-   // cout<<"marimea long in memorie este de:"<<sizeof(long)<<"bytes"<<endl;
-   // cout<<"marimea bool in memorie este de:"<<sizeof(bool)<<"bytes"<<endl;
-   // cout<<"marimea unsigned int in memorie este de:"<<sizeof(unsigned int)<<"bytes"<<endl;
+    //operatori de atribuire complecsi
+    //+=, -=, *=, /=, %=
+  //v=v*3
+   // v*=3;
+    //cout<<v<<endl;
+    //g+=2;
+    //cout<<g<<endl;
+   //h-=6;
+    //cout<<h<<endl;
+    
+    //operatori de incrementare/decrementare ++/--
+    //v++;
+    //g++;
+    //cout<<v<<endl<<g<<endl;
+    
+    //v=++h;//v ia valoarea lui h,apoi h creste cu 1;
+    //v=++h;//mai intai valoarea lui h creste cu 1,apoi v ia valoarea li h;
+    //cout<<v<<endl<<h<<endl;
+    
+    //v=h--;//<=> v=h; h=h-1;
+   // v=--h; //<=> h=h-1; v=h;
+    //cout<<v<<endl<<h<<endl;
+    
+    //operatorul conditional ? => expresie ? val1 : val2
+    //cout<<(v%2 == 0 ? "par" : "impar");
+    
+    //interschimbarea a 2 variabile
+    //1: folosind o variabila auxiliara
+    // cout<< g <<" "<<v<<endl;
+    // int aux;
+    //aux = g;
+    //g = v;
+    //v = aux;
+    //cout << g <<" "<<v<<endl;
+    
+    
+    
+    //2: prin adunari si scaderi succesive
+    ///cout << h <<" "<<v<<endl;
+    //v = v+h;// v1 = suma
+    //h = v-h;// v2=suma - v2
+    //v = v-h;// v1 = suma - v2
+    //cout << h <<" "<<v<<endl;
+    
+    /*if(expresie)
+    ....
+    else
+    ...
+    */
+     
+     /*if(h%2){
+         cout<<"impar";
+     }
+     else{
+         cout<<"par";
+     }
+    */
+    
+    //if(v)
+    //cout<<"nenul";
+    //else
+   //cout<<"nul";
    
-   //cout<<"salut"<<endl<<"mere\n"<<"pere";
-   char ccc = 'A';
-   int nr
-   cout<<(char)nr;
-//    cout<<"introduceti un numar:";
-//    cin>>i;
-//    cout<<i;
+   /*if (v<h)               
+   cout<<"v<h"<<endl;
+   else
+   cout<<"v>h"<<endl;
+   
+   if(v<g)
+    cout<<"v<g"<<endl;
+   else
+    cout<<"v>g"<<endl;
+   if(v=h)
+    cout<<"v=h"<<endl;
+   else
+    cout<<"v /= h"<<endl;
+    */
     
-/*    for(i=1;i<5;i++)
-    {
-        if(i==2)
-        {
-            i++;
-        }
+    
+    if(v > g){
+      if(h>v)
+      cout<<"h este maximul ";
+    else
+        cout<<" v este maximul ";
+        
     }
-*/
+  else{
+      if(h>g)
+        cout<<"h este maximul";
+    else
+        cout<<"g este maximul";
+  }  
+    
+    if((v>g)&&(h>v))
+        cout<<"h ste maximul";
+    if((v>g)&&!(h>v))
+        cout<<"v este maximul";
+    
+    
+    
+    
+    
     return 0;
 }
-
