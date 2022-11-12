991aece1ddeca13d44346fbee21b0de971c983b3
Gusion2045 <114810619+Gusion2045@users.noreply.github.com>
2022-11-12 15:52:05 +0200

    Add files via upload
    lectiile 6 si 7 c++

--- main.cpp
+++ main.cpp
@@ -7,118 +7,133 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    int v, g, h;
-    v = 1;
-    g = 7;
-    h = 5;
-    
-    //cout<<v<<endl<<g<<endl<<h<<endl;
-    
-    //operatori de atribuire complecsi
-    //+=, -=, *=, /=, %=
-  //v=v*3
-   // v*=3;
-    //cout<<v<<endl;
-    //g+=2;
-    //cout<<g<<endl;
-   //h-=6;
-    //cout<<h<<endl;
-    
-    //operatori de incrementare/decrementare ++/--
-    //v++;
-    //g++;
-    //cout<<v<<endl<<g<<endl;
-    
-    //v=++h;//v ia valoarea lui h,apoi h creste cu 1;
-    //v=++h;//mai intai valoarea lui h creste cu 1,apoi v ia valoarea li h;
-    //cout<<v<<endl<<h<<endl;
-    
-    //v=h--;//<=> v=h; h=h-1;
-   // v=--h; //<=> h=h-1; v=h;
-    //cout<<v<<endl<<h<<endl;
-    
-    //operatorul conditional ? => expresie ? val1 : val2
-    //cout<<(v%2 == 0 ? "par" : "impar");
-    
-    //interschimbarea a 2 variabile
-    //1: folosind o variabila auxiliara
-    // cout<< g <<" "<<v<<endl;
-    // int aux;
-    //aux = g;
-    //g = v;
-    //v = aux;
-    //cout << g <<" "<<v<<endl;
-    
-    
-    
-    //2: prin adunari si scaderi succesive
-    ///cout << h <<" "<<v<<endl;
-    //v = v+h;// v1 = suma
-    //h = v-h;// v2=suma - v2
-    //v = v-h;// v1 = suma - v2
-    //cout << h <<" "<<v<<endl;
-    
-    /*if(expresie)
-    ....
-    else
-    ...
-    */
+//  int n;
+//  cout<<"n= ";
+//  cin>>n;
+//  //for(int i=1; i<= n;i++){
+//      for(int j=1;j<=i;j++){
+//         cout<<"*";
+//          cout<<j;
      
-     /*if(h%2){
-         cout<<"impar";
-     }
-     else{
-         cout<<"par";
-     }
-    */
-    
-    //if(v)
-    //cout<<"nenul";
-    //else
-   //cout<<"nul";
-   
-   /*if (v<h)               
-   cout<<"v<h"<<endl;
-   else
-   cout<<"v>h"<<endl;
-   
-   if(v<g)
-    cout<<"v<g"<<endl;
-   else
-    cout<<"v>g"<<endl;
-   if(v=h)
-    cout<<"v=h"<<endl;
-   else
-    cout<<"v /= h"<<endl;
-    */
-    
-    
-    if(v > g){
-      if(h>v)
-      cout<<"h este maximul ";
-    else
-        cout<<" v este maximul ";
         
-    }
-  else{
-      if(h>g)
-        cout<<"h este maximul";
-    else
-        cout<<"g este maximul";
-  }  
+        
     
-    if((v>g)&&(h>v))
-        cout<<"h ste maximul";
-    if((v>g)&&!(h>v))
-        cout<<"v este maximul";
+//     cout<<endl;
     
+//     int a=0;
     
+//     for (int i=1;i<=n;i++){
+//         for(int j=1; j<=i;j++){
+//             cout<<a;
+//             a=a+1;
+//         }
+//         cout<<endl;
+//     }
     
+ //int a, b ,c ,d, e;
+// if(a==0)
+// {
+//     if(b==0){
+//         if(c==0){
+//             if(d==0){
+//                 if(e==0){
+//                     cout<<"Final!"
+//                 }
+//                 cout<<"s-a blocat la D";
+//             }
+//             cout<<"s-a blocat la  C";
+//         }
+//          cout<<"s-a blocat la  B";
+//     }
+//      cout<<"s-a blocat la  A";
+// }
+  
+    // if(a!=0)
+    // {
+    //   cout<<"s-a blocat la  a";
+    //   return;
+    // }
+    // if(b!=0)
+    // {
+    //   cout<<"s-a blocat la  b";
+    //   return;
+    // }
+    // if(c!=0)
+    // {
+    //   cout<<"s-a blocat la  c";
+    //   return;
+    // }
+    // if(e!=0)
+    // {
+    //   cout<<"s-a blocat la  d";
+    //   return;
+    // }
+    // if(e!=0)
+    // {
+    //   cout<<"s-a blocat la  e";
+    //   return;
+    // }
+    // cout<<"final";
     
+    // int a, b, i;
+    // cin>>a;
+    // cin>>b;
+    //  if(b<a){
+    //      int aux;
+    //      aux=a;
+    //      a=b;
+    //      b=aux;
+    //  }
+    // for(i=a;i<=b;i++){
+    //     if(i%2==1){
+            
+    //       cout<< " " <<i; 
+    //     }
     
-    return 0;
+    //  }
+     
+    // int n,s=0;
+    // cout<<"n=";
+    // cin>>n;
+    // while(n!=0){
+    //     s+=n%10;
+    //     n/=10;
+    // }
+    // cout<<"Suma= "<<s;
+    
+
+// int cifra,intrus,n,nr_aparitii=0;
+// cout<<"n= ";
+// cin>>n;
+// cout<<"Intrus = ";
+// cin>>intrus;
+// while(n!=0){
+//     cifra=n%10;
+//     if(cifra==intrus){
+//         nr_aparitii++;
+     
+//     }
+//     n/=10;
+// }
+
+//  cout<<nr_aparitii;
+
+int n,max=-1;
+cin>>n;
+while(n!=0)
+{
+int c=n%10;
+if(c>max)max=c;
+n=n/10;
+}
+cout<<max;
+return 0;
+
+  return 0;
+
 }
